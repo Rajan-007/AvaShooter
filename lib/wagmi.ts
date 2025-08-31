0dbc83c6ccc782b986aca6a2517870354a345214
Rajan <aravinthrajan034@gmail.com>
2025-08-31 11:46:02 +0530

    fuji network

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,6 +1,7 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
+  avalancheFuji,
   baseSepolia,
   sepolia,
   somniaTestnet,
@@ -10,10 +11,11 @@ export const config = getDefaultConfig({
   appName: 'RainbowKit demo',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
+    avalancheFuji,
     baseSepolia,
     sepolia,
     somniaTestnet,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [somniaTestnet] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [avalancheFuji] : []),
   ],
   ssr: false,
 });
--- starkshoot-server/src/index.ts
+++ starkshoot-server/src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001','https://ava-shooter-agyr.vercel.app/'],
   credentials: true
 }));
 
